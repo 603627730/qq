229c123d66809e65e20dee499f23125ed83ac8e4
yantonghui <603627730@qq.com>
2022-06-06 22:38:55 +0800

    time

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,86 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <input v-model="message" style="text-align: left"  placeholder="xx市或者xx区，例如：城阳区">
+    <button v-on:click="getinfo()">查询</button>
+    <p>查询时间 ： {{timeis}}</p>
+
+  </div>
+  <div
+      class="echart"
+      id="mychart"
+      :style="{ float: 'left', width: '30%', height: '400px' }"
+  >
+
   </div>
 </template>
 
+
 <script>
+
+import * as echarts from "echarts";
+import axios from 'axios'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      message : '城阳区',
+      input: '',
+      timeis : '',
+      name: "张雪",
+      xData: ["2020-02", "2020-03", "2020-04", "2020-05"], //横坐标数据
+      yData: [120, 132], //纵坐标数据，与横坐标对应
+      ainfo :[]
+    };
+  },
+  mounted() {
+
+    this.getinfo()
+  },
+  methods: {
+
+    getinfo(){
+      axios
+          .get('http://139.196.6.43:8000/file/readExcel?city=' + this.message)
+          .then(response => (
+              this.ainfo = response.data,
+                  console.log(this.ainfo.temperature),
+                  this.initEcharts(),
+                  this.timeis = this.ainfo.reporttime
+          ))
+    },
+
+    initEcharts() {
+      console.log("this.ainfo.temperature")
+
+      const option = {
+        title: {
+          text: this.ainfo.province + "省" + this.ainfo.city + "天气情况"
+        },
+        tooltip: {},
+        legend: {
+          data: [ "天气状况"]
+        },
+        xAxis: {
+          data: [ "温度","湿度"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "天气状况",
+            type: "bar", //类型为柱状图
+            data: [parseInt(this.ainfo.temperature), parseInt(this.ainfo.humidity)]
+          }
+        ]
+      };
+      const myChart = echarts.init(document.getElementById("mychart"));// 图标初始化
+      myChart.setOption(option);// 渲染页面
+      //随着屏幕大小调节图表
+      window.addEventListener("resize", () => {
+        myChart.resize();
+      });
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
